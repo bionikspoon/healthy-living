ae1be4360452486ff547c4ea42a6365b811cccbe
Manu <bionikspoon@gmail.com>
2015-06-03 01:56:38 -0500

    node-sass cleanup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 'use strict';
+var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static('dist'));
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip(path.join(__dirname, "dist")));
 
 app.listen(port);
