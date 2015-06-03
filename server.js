058ba8c0ea61146c31a31dd780ff3f629de962ee
Manu <bionikspoon@gmail.com>
2015-06-03 10:23:16 -0500

    removed gzippo

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
-var gzippo = require('gzippo');
+
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip(path.join(__dirname, "dist")));
+app.use(express.static('dist'));
 
 app.listen(port);
