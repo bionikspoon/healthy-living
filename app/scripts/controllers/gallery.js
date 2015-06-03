820972ae290d547a22416c5256b3b92e150b2927
Manu <bionikspoon@gmail.com>
2015-06-02 21:25:59 -0500

    gallery view

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -7,11 +7,43 @@
  * # GalleryCtrl
  * Controller of the healthyLivingApp
  */
-angular.module('healthyLivingApp')
-  .controller('GalleryCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('healthyLivingApp').controller('GalleryCtrl', function ($scope) {
+  $scope.pictures = [];
+  $scope.rate = 0;
+  $scope.max = 10;
+  $scope.isReadonly = false;
+  var picturesUrl = 'http://lorempixel.com/300/180';
+
+  var titles = [
+    'Healthy Food',
+    'Healthy @ Work',
+    'City Life',
+    'Staying Fit',
+    'Looking Good',
+    'Nightlife!!'
+  ];
+
+  var keywords = [
+    'food',
+    'business',
+    'city',
+    'sports',
+    'fashion',
+    'nightlife'
+  ];
+
+  var dummyText = 'Lorem	ipsum	dolor	sit	amet,	consectetur	adipiscing elit.	Sed	sed	erat	turpis.	Integer	eget consectetur	quam.	Sed	at	quam	ut	dolor	varius condimentum	et	sit	amet	odio.';
+
+  $scope.addPics = function (index) {
+    $scope.pictures.push({
+      url: picturesUrl + '/' + keywords[index],
+      title: titles[index],
+      summary: dummyText
+    });
+  };
+  var i;
+
+  for (i = 0; i <= 5; i++) {
+    $scope.addPics(i);
+  }
+});
